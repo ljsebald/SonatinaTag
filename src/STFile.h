03a29a11204e107860aef3942632483159141ff7
Lawrence Sebald <bluecrab2887@netscape.net>
2010-06-05 23:12:38 -0400

    Add missing license block...

--- src/STFile.h
+++ src/STFile.h
@@ -1,10 +1,20 @@
-//
-//  STFile.h
-//  SonatinaTag
-//
-//  Created by Lawrence Sebald on 6/5/10.
-//  Copyright 2010 Lawrence Sebald. All rights reserved.
-//
+/*
+    SonatinaTag
+    Copyright (C) 2010 Lawrence Sebald
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License version 2.1 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 #ifndef STFile_h
 #define STFile_h
