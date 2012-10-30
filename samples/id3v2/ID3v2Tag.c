2cd32a41abf66d3bf7d901aedb63ac0c4384fc81
Lawrence Sebald <bluecrab2887@netscape.net>
2012-10-30 19:44:11 -0400

    Fix the ID3v2 tag example program...

--- samples/id3v2/ID3v2Tag.c
+++ samples/id3v2/ID3v2Tag.c
@@ -17,7 +17,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
 */
 
-/*  This example program reads the tags from a FLAC file, and uses a block to
+/*  This example program reads ID3v2 tags from a MP3 file, and uses a block to
     print them out. If blocks are not supported by the compiler, it will do so
     using a callback instead.
 */
@@ -35,9 +35,9 @@ void tag_cb(const ST_Dict *d, void *data, const void *k, const void *v) {
     char fourcc[8] = { *key >> 24, *key >> 16, *key >> 8, *key, 0 };
     ST_Frame *f = (ST_Frame *)v;
 
-    printf("Key = \"%s\"", fourcc);
+    printf("Key = \"%s\" ", fourcc);
 
-    if(ST_Frame_type(f) == ST_FrameType_Text) {
+    if(ST_ID3v2_Frame_type(f) == ST_FrameType_Text) {
         ST_TextFrame *tf = (ST_TextFrame *)v;
 
         if(ST_ID3v2_TextFrame_encoding(tf) == ST_TextEncoding_ISO8859_1 ||
@@ -49,12 +49,12 @@ void tag_cb(const ST_Dict *d, void *data, const void *k, const void *v) {
             printf("Value = \"%s\"", (char *)buf);
         }
     }
-    else if(ST_Frame_type(f) == ST_FrameType_UserText) {
+    else if(ST_ID3v2_Frame_type(f) == ST_FrameType_UserText) {
         ST_UserTextFrame *utf = (ST_UserTextFrame *)v;
 
         if(ST_ID3v2_UserTextFrame_encoding(utf) == ST_TextEncoding_ISO8859_1 ||
            ST_ID3v2_UserTextFrame_encoding(utf) == ST_TextEncoding_UTF8) {
-            size_t len = ST_ID3v2_UserTextFrame_textlength(utf);
+            size_t len = ST_ID3v2_UserTextFrame_textLength(utf);
             size_t len2 = ST_ID3v2_UserTextFrame_descLength(utf);
             uint8_t buf[len + 1];
             uint8_t buf2[len2 + 1];
@@ -64,6 +64,37 @@ void tag_cb(const ST_Dict *d, void *data, const void *k, const void *v) {
             printf("Description = \"%s\" Value = \"%s\"", (char *)buf2,
                    (char *)buf);
         }
+    }
+    else if(ST_ID3v2_Frame_type(f) == ST_FrameType_URL) {
+        ST_URLFrame *uf = (ST_URLFrame *)v;
+        size_t len = ST_ID3v2_URLFrame_length(uf);
+        uint8_t buf[len + 1];
+
+        memcpy(buf, ST_ID3v2_URLFrame_URL(uf), len);
+        buf[len] = 0;
+
+        printf("Value = \"%s\"", (char *)buf);
+    }
+    else if(ST_ID3v2_Frame_type(f) == ST_FrameType_UserURL) {
+        ST_UserURLFrame *uuf = (ST_UserURLFrame *)v;
+        ST_TextEncoding e = ST_ID3v2_UserURLFrame_descEncoding(uuf);
+        size_t len = ST_ID3v2_UserURLFrame_URLLength(uuf);
+        uint8_t buf[len + 1];
+
+        memcpy(buf, ST_ID3v2_UserURLFrame_URL(uuf), len);
+        buf[len] = 0;
+
+        if(e == ST_TextEncoding_ISO8859_1 || e == ST_TextEncoding_UTF8) {
+            size_t len2 = ST_ID3v2_UserURLFrame_descLength(uuf);
+            uint8_t buf2[len2 + 1];
+
+            memcpy(buf2, ST_ID3v2_UserURLFrame_desc(uuf), len2);
+            buf2[len2] = 0;
+
+            printf("Description = \"%s\" ", (char *)buf2);
+        }
+
+        printf("Value = \"%s\"", (char *)buf);
     }
 
     printf("\n");
